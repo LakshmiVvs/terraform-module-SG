31d4931dce3d6a8cd0c348f7429c3f3679ffdd76
LakshmiVvs <vvslakshmi791@gmail.com>
2025-11-04 15:30:59 +0530

    terraform

---
+++ variable.tf
@@ -0,0 +1,22 @@
+variable "sg_name" {
+    type = string
+}
+
+variable "environment" {
+    type = string
+}
+
+variable "sg_name" {
+    type = string
+}
+
+variable "description" {
+    type = string
+    default = ""
+}
+
+variable "sg_tags" {
+    type = map
+    default = {}
+}
+
